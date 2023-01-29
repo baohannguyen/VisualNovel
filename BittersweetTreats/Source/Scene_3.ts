971ca7b83db906e37beca22df42b3d17d03b8851
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2023-01-29 21:19:45 +0100

    switch text to novel page

--- BittersweetTreats/Source/Scene_3.ts
+++ BittersweetTreats/Source/Scene_3.ts
@@ -2,15 +2,11 @@ namespace Novel {
     export async function scene_3(): ƒS.SceneReturn {
         console.log("Scene 3");
 
-        let text = {
-            narrator: {
-                TX01: "Innerhalb einer Woche hat sich Celeste für die Stelle beworben. Nach ein paar Tagen hat sie eine Antwort bekommen und muss, um die Stelle zu kriegen Probearbeiten."
-            }
-        };
-
+        ƒS.Speech.hide();
         await ƒS.Location.show(locations.blackScreen);
+        await ƒS.update(transition.stripes.duration, transition.stripes.alpha, transition.stripes.edge);
         await ƒS.update();
-        await ƒS.Speech.tell(characters.narrator, text.narrator.TX01);
+        await ƒS.Text.print("Innerhalb einer Woche hat sich Celeste für die Stelle beworben. Nach ein paar Tagen hat sie eine Antwort bekommen und muss, um die Stelle zu kriegen Probearbeiten.");
         
     }
 }
